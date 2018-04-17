fb682a0f104e54eed6a68030e268766f4e9746ce
Steven Bouwkamp <bouwkast@mail.gvsu.edu>
2018-04-17 18:34:24 -0400

    Add example commands

---
+++ ExtensibleDiscordBot/CommandModules/ExampleModule.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace ExtensibleDiscordBot.CommandModules
+{
+    [Name("Example")]
+    public class ExampleModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("example")]
+        [Summary("This is the summary for the example command.")]
+        public async Task Example(IGuildUser user)
+        {
+            await ReplyAsync("Hi, I'm an example.");
+        }
+
+        [Command("ping")]
+        [Summary("This is the summary for the ping command.")]
+        public async Task Example(string input)
+        {
+            await ReplyAsync($"Hey {Context.User.Mention}, you typed {input}!");
+        }
+
+        [Command("example")]
+        [Summary("This is the summary for the ping command.")]
+        [RequireUserPermission(GuildPermission.Administrator)]
+        public async Task AdminExample()
+        {
+            await ReplyAsync($"Hey {Context.User.Mention}, you are an admin!");
+        }
+    }
+}
